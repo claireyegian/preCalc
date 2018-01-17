1b8b39c5ba9ade01279eb3f581bd4ee461f0cadb
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-01-17 11:56:01 -0500

    Updated from Brython Server: 1/17/2018 11:56:02 AM

--- ifDemo.py
+++ ifDemo.py
@@ -6,3 +6,9 @@ grade = float(input("What was your grade? "))
 
 if grade >= 90:
     print('You earned an A')
+elif grade >= 80:
+    print('You earned a B')
+elif grade >= 70:
+    print('You earned a C')
+elif grade >= 60:
+    print('You earned a D')
