8ee25b084564501169e85c83def49c00b5fe39ca
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-01-18 12:57:02 -0500

    Updated from Brython Server: 1/18/2018 12:57:01 PM

--- loopDemo.py
+++ loopDemo.py
@@ -2,10 +2,16 @@
 #1/18/18
 #loopDemo.py - how to use a loop
 
-for i in range(1,101):
-    print(i)
+"""for i in range(1,101):
+    if '7' in str(i) or i%7==0:
+        print('Buzz')
+    else:
+        print(i)"""
 
 i = 1
 while i <= 100:
-    print(i)
+    if '7' in str(i) or i%7==0:
+        print('Buzz')
+    else:
+        print(i)
     i += 1
