167c8fe0154e9644747e1f72088b879f01eb91a1
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-04-23 09:45:43 -0700

    Updated from Brython Server: 4/23/2018 12:45:53 PM

--- jarProblem.py
+++ jarProblem.py
@@ -1,1 +1,6 @@
+#Claire Yegian and Lea Adams-Blackmore
+#4/23/18
+#jarProblem.py - calculates fair costs for different strategies of jar game
 
+N = int(input('Enter the number of marbles: '))
+W = int(input('Enter the monetary prize: '))
