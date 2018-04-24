12f56e36a67c949077c879953babce944c6bfbea
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-04-24 09:47:10 -0700

    Updated from Brython Server: 4/24/2018 12:47:18 PM

--- jarProblemSimulation.py
+++ jarProblemSimulation.py
@@ -1,1 +1,24 @@
+#Claire Yegian and Lea Adams-Blackmore
+#4/24/18
+#jarProblemSimulation.py - simulations to test jar problem answers
 
+from random import randint
+
+guess = input('Enter your guess (red or green: ')
+N = int(input('Enter the number of marbles: '))
+W = int(input('Enter the monetary prize: '))
+red = randint(1,N)
+green = N - red
+
+marbleList = []
+item = 0
+while item <= red:
+    marbleList.append(1)
+    item += 1
+
+item2 = 0
+while item2 <= green:
+    marbleList.append(2)
+    item2 += 1
+
+print(marbleList)
