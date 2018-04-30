72de37774b61c08d5e2fef836ba3f462e9e51127
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-04-29 20:13:53 -0400

    Update jarProblemSimulation.py

--- jarProblemSimulation.py
+++ jarProblemSimulation.py
@@ -43,10 +43,10 @@ N = 20
 W = 10
 wins1 = 0
 runs1 = 0
-while runs1 <= 20:
+while runs1 <= 100:
     red1 = randint(0,N)
     green1 = N - red1
-    print(red1,green1)
+    
     marbleList1 = []
     item1 = 0
     while item1 < green1:
@@ -57,12 +57,15 @@ while runs1 <= 20:
     while item3 < red1:
         marbleList1.append(1)
         item3 += 1
-    print(marbleList1)
+    print(marbleList1, len(marbleList1))
+    
     guess1 = marbleList1[randint(0,N-1)]
     marblePicked1 = marbleList1[randint(0,N-1)]
-    print(guess1,marblePicked1)
+    
     if guess1 == marblePicked1:
         wins1 += 1
-    
+    print(guess1, marblePicked1)
+    print(wins1)
+
     runs1 += 1
 print(wins1)
