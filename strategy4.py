d06c40108cbbcf2e8d071d3d25067266ce2e1875
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-05-01 22:43:36 -0400

    Updated from Brython Server: 5/1/2018 10:43:35 PM

--- strategy4.py
+++ strategy4.py
@@ -1,1 +1,11 @@
+#Claire Yegian and Lea Adams-Blackmore
+#4/30/18
+#strategy4.py - finds exact value for R and D and creates simulation for strategy 4 (pay R above D)
 
+from random import randint
+
+#Exact value
+
+#Simulation
+
+print('You won',wins,'times out of 1000 or',wins/10,'percent of the time.')
